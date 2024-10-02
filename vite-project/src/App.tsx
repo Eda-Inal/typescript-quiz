1ee469e67f98f235ba27bf87d705e76a352152c4
Eda Işıl İnal <edainalisil@gmail.com>
2024-10-02 20:25:03 +0300

    add btns design

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -13,18 +13,41 @@ function App() {
           <h1>Quiz Game</h1>
           <h4>Score:25</h4>
         </div>
-     <div className='timer'>
-     <div className='circle'>
-      <h3>20</h3>
-     </div>
-     </div>
-     <div className='question-part'>
-<h2>What is the capital city of German?</h2>
-<div className='buttons'>
+        <div className='timer'>
+          <div className='circle'>
+            <h3>20</h3>
+          </div>
+        </div>
+        <div className='question-part'>
+          <h2>What is the capital city of German?</h2>
+          <div className='buttons'>
+            <div className='button-row'>
+              <button className='primary-btn btn-content'>
+                <div>A)</div>
+                <div>Paris</div>
+              </button>
+              <button className='secondary-btn btn-content'>
+                <div>B)</div>
+                <div>Berlin</div>
+              </button>
 
-</div>
-     </div>
-         
+            </div>
+            <div className='button-row'>
+              <button className='primary-btn btn-content'>
+                <div>C)</div>
+                <div>Belgium</div>
+              </button>
+
+
+              <button className='primary-btn btn-content'>
+                <div>D)</div>
+                <div>Washington</div>
+              </button>
+
+            </div>
+          </div>
+        </div>
+
 
 
       </div>
