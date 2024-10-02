6ade547dfd99941665c6dd3c056d2cc2c9b1b0e2
Eda Işıl İnal <edainalisil@gmail.com>
2024-10-02 12:25:03 +0300

    add top area score,q number and header

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,22 @@
 import './styles.scss';
 
+
 function App() {
 
 
   return (
     <>
-  
- 
+
+      <div className="container">
+        <div className="top-area">
+          <h4>Q:1/5</h4>
+          <h1>Quiz Game</h1>
+          <h4>Score:25</h4>
+        </div>
+
+
+      </div>
+
     </>
   )
 }
