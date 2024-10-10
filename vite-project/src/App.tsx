b3f80f6d31c46611446c8e4dc3b6ab867ef01f91
Eda Işıl İnal <edainalisil@gmail.com>
2024-10-10 17:09:10 +0300

    add score messages and did responsive design score table

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -126,9 +126,24 @@ console.log("answer",answerControl);
           finishQuiz &&(
 <div className='score'>
         <div className='score-card'>
-          <h3>Your score is 5 out of 10</h3>
-
-          <h4>Keep practicing</h4>
+          <div className='score-card-messages'>
+            <div>
+              
+            </div>
+          <h3 className='h3-message'>Your score is <span className='span-color'>{score}</span> out of {datasLength *10}</h3>
+       
+       {
+         score >= 70? (
+           <h4>Amazing job! You really nailed it with a score of {score}. Keep up the great work, you're on your way to becoming a React pro!</h4>
+         ): score >= 50 ? (
+           <h4>Great effort! You scored {score}, which is a solid result. With a bit more practice, you'll reach the top in no time!</h4>
+         ) : (
+           <h4>Don't give up! You scored {score}, but remember that every step is progress. Keep learning and you'll see improvement soon!</h4>
+         )
+       }
+          </div>
+        
+          
 
         </div>
         <button className='try-again-btn' onClick={() => setFinishQuiz(false)}>Try again</button>
