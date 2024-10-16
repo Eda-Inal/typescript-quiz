3b837e71273566e59480ce4d2e4ab5703a231b45
Eda Işıl İnal <edainalisil@gmail.com>
2024-10-16 20:16:16 +0300

    created timer

--- vite-project/src/components/Timer.tsx
+++ vite-project/src/components/Timer.tsx
@@ -1,11 +1,34 @@
+import { useState, useEffect } from "react"
+import { useQuizContext } from '../Context'
+import { QuizState } from "./Interfaces";
+
+
+
 function Timer() {
+  const { setQuiz } = useQuizContext();
+  const [seconds, setSeconds] = useState(50);
+
+  useEffect(() => {
+    if (seconds > 0) {
+      const interval = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds - 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [seconds]);
+  useEffect(() => {
+    if (seconds === 0) {
+      setQuiz((prevQuiz: QuizState) => ({ ...prevQuiz, finishQuiz: true }));
+    }
+  }, [seconds]);
   return (
     <>
       <div className='timer'>
-        <div className='circle'>
-          <h3>30</h3>
-        </div>
+      <div className='circle'>
+        <h3 className='seconds-text'>{seconds}</h3>
       </div>
+    </div>
     </>
   )
 }
